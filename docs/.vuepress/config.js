b73a52dcb7fa73b25e1ddd8f8a6d2e9953e517f9
davechai <davecqx@gmail.com>
2019-05-15 17:34:10 +0800

    add test markdown files

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: '洗心革面',
+  description: '我们在这世界上不停地奔跑，一不小心就改变了生活',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ]
+  }
+}
