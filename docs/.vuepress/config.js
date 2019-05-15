77001c6b379b8c50daa2b82fb34767ad7e07862b
davechai <davecqx@gmail.com>
2019-05-15 22:55:31 +0800

    add YAML front matter

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,9 +3,17 @@ module.exports = {
   description: '我们在这世界上不停地奔跑，一不小心就改变了生活',
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/guide/' },
-      { text: 'External', link: 'https://google.com' },
-    ]
+      { text: '首页', link: '/' },
+      { text: '技术', link: '/tech/' },
+      { text: '书籍', link: '/book/' },
+      { text: 'Github', link: 'https://github.com/caicaishmily' },
+    ],
+    search: false,
+    sidebar: {
+      '/tech/': [
+        '',     /* /foo/ */
+        'flutter',  /* /foo/one.html */
+      ],
+    }
   }
 }
---
+++ docs/.vuepress/tech/README.md
@@ -0,0 +1,1 @@
+# [Flutter](/tech/flutter.md)
